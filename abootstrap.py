e6c31af95e5d56e16fc40941462c3e8205dec077
Daniel Wallace <danielwallace@gtmanfred.com>
2013-07-15 20:56:54 -0500

    Add argparser to set mirrorlist and root path

--- abootstrap.py
+++ abootstrap.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function, generators, with_statement
+import argparse
 from time import sleep
 import os, re, tarfile
 from subprocess import call
@@ -12,6 +13,7 @@ except ImportError:
     from urllib import urlretrieve
 
 def base_system(mirror, rootpath='/mnt/', devel=0):
+    devel = 0
     arch = os.uname()[-1]
     if os.path.isfile(mirror):
         db = Repo(mirror)
@@ -22,15 +24,12 @@ def base_system(mirror, rootpath='/mnt/', devel=0):
     base_packages = db.group_members('base')
     base_packages = set(remove_v_r(p) for p in base_packages)
     base_depends = db.depends(base_packages)
-    #print(len(base_packages), len(base_depends), len(base_packages | base_depends))
-    #print(base_packages)
-    #print(base_depends)
     print('\n'.join(base_packages | base_depends))
     return
     
     cache_location = '/'.join([rootpath, 'var/cache/pacman/pkg/'])
     # exist_ok is not in py2
-    #os.makedirs(cache_location, exist_ok=1)
+    os.makedirs(cache_location, exist_ok=1)
 
     for pkg in base_packages | base_depends:
         filename = '{}-{}-{}.pkg.tar.xz'.format(db[pkg]['NAME'], db[pkg]['VERSION'], db[pkg]['ARCH'])
@@ -56,18 +55,16 @@ def base_system(mirror, rootpath='/mnt/', devel=0):
 
 
 if __name__ == '__main__':
-    #print( base_system('http://dfw.mirror.rackspace.com/archlinux/'))
-    #d = DescParse(open('/var/lib/pacman/local/pacman-4.1.2-1/desc'))
-    #print(d.info)
-    #db = Repo('/var/lib/pacman/sync/core.db')
-    #print(db.tree)
-    #print(db['pacman-4.1.2-1'].serialize('NAME VERSION DESC URL ARCH BUILDDATE INSTALLDATE PACKAGER SIZE REASON GROUPS LICENSE VALIDATION REPLACES DEPENDS OPTDEPENDS CONFLICTS PROVIDES'.split()))
-    #print(db['pacman-4.1.2'])
-    #print(db['pacman'])
-    #print(db['pacman-contrib'])
-    #base_system('/var/lib/pacman/sync/core.db')
-    #base_system('http://dfw.mirror.rackspace.com/archlinux/')
-    base_system('/tmp/coredb')
+    parser = argparse.ArgumentParser(description='Basic commands')
+    parser.add_argument( '-m',  '--mirror', nargs=1,
+                        default="http://mirrors.kernel.org/archlinux/", 
+                        help='Mirror to download from')
+    parser.add_argument( '-r', '--root', nargs=1,
+                        default='/mnt', help='Destination to install to')
+    parser.add_argument( '-d', '--devel', nargs=1, 
+                         default=0, help='And base-devel')
+    args = parser.parse_args()
+    base_system(args.mirror[0], args.root[0], args.devel)
 
 
 # vime: set ts=4 ws=4 et
